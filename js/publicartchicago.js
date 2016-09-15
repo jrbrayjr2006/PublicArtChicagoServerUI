3d5c7b5aec3edb6b8aa842e958bbbb6ccdd52ea3
jrbrayjr2006 <jrbrayjr2006@gmail.com>
2016-09-14 19:56:22 -0500

    Added display tables to content areas

--- js/publicartchicago.js
+++ js/publicartchicago.js
@@ -3,6 +3,8 @@
  */
 
 var app = angular.module('publicArt', ["ngRoute"]);
+var urlPrefix = "/PublicArtChicago";
+//var urlPrefix = "/PAC-dev";
 
 var loginApp = angular.module('loginApp', ["ngRoute"]);
 
@@ -12,14 +14,15 @@ app.config(function($routeProvider) {
         templateUrl: "/PublicArtChicago/login.html",
         controller: "loginController"
     })
-    .when("/PublicArtChicago/logout", {templateUrl: "/PublicArtChicago/index.html"})
-    .when("/artists", {templateUrl: "/PublicArtChicago/view/artists.html"})
-    .when("/objects", {templateUrl: "/PublicArtChicago/view/objects.html"})
-    .when("/ads", {templateUrl: "/PublicArtChicago/view/ads.html"})
-    .when("/tours", {templateUrl: "/PublicArtChicago/view/tours.html"})
-    .when("/pois", {templateUrl: "/PublicArtChicago/view/pois.html"})
-    .when("/narrators", {templateUrl: "/PublicArtChicago/view/narrators.html"})
-    .otherwise({templateUrl: "/PublicArtChicago/view/welcome.html"});
+    .when("/PublicArtChicago/logout", {templateUrl: urlPrefix + "/index.html"})
+    .when("/artists", {templateUrl: urlPrefix + "/view/artists.html"})
+    .when("/objects", {templateUrl: urlPrefix + "/view/objects.html"})
+    .when("/ads", {templateUrl: urlPrefix + "/view/ads.html"})
+    .when("/tours", {templateUrl: urlPrefix + "/view/tours.html"})
+    .when("/pois", {templateUrl: urlPrefix + "/view/pois.html"})
+    .when("/narrators", {templateUrl: urlPrefix + "/view/narrators.html"})
+    .when("/createObject", {templateUrl: urlPrefix + "/view/createObject.html"})
+    .otherwise({templateUrl: urlPrefix + "/view/welcome.html"});
 })
 .service('loginService', function($http, $q, $log) {
     console.debug("loginService...");
@@ -32,6 +35,9 @@ app.config(function($routeProvider) {
 .service('dashboardService', function($http, $q, $log) {
     console.debug("dashboardService...");
     //
+})
+.service('parseService', function($http, $q, $log){
+    console.debug("ENTER:: parseService...");
 })
 .controller('titleController', function($log, $scope) {
     $scope.loginTitle = "Public Art Chicago Login";
@@ -49,7 +55,7 @@ app.config(function($routeProvider) {
 })
 .controller('dashboardController', function($log, $scope, dashboardService) {
     $scope.dashboardTitle = 'Public Art Chicago Dashboard';
-    //
+    $scope.baseUrl = urlPrefix;
 
     $scope.search = function() {
         console.debug("search()...");
@@ -59,5 +65,27 @@ app.config(function($routeProvider) {
         console.debug("logout()...");
         //alert("Logging out now!");
         location.href = 'login.html';
+    };
+
+    /**
+     * Is used by ng-click directive in button element to navigate to new view
+     */
+    $scope.openObjectList = function() {
+        console.debug("dashboardController.openObjectList()...");
+        location.href = urlPrefix + '/dashboard.html#/objects';
+    };
+
+    $scope.openCreateObject = function() {
+        console.debug("dashboardController.openCreateObject()...");
+        location.href = urlPrefix + '/dashboard.html#/createObject';
+    };
+})
+.controller('modalController', function($uibModal, $log, $scope){
+    console.debug("modalController");
+    var $modalCtrl = this;
+
+    $modalCtrl.openCreateObjectModal = function() {
+        console.debug("openCreateObjectModal()...");
+        //
     };
 });
